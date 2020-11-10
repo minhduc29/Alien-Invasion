a81ba5d092cd23679cd74addd16b9076139c67d8
Nguyen Minh DUc <ngmductm@gmail.com>
2020-11-11 06:14:10 +0700

    initialize AI player and add automatic fire bullet

---
+++ ai_player.py
@@ -0,0 +1,26 @@
+from alien_invasion import AlienInvasion
+
+class AIPlayer:
+    def __init__(self, ai_game):
+        """Automatic player for Alien Invasion"""
+        self.ai_game = ai_game
+
+    def run_game(self):
+        """Replace the original run_game()"""
+        self.ai_game.stats.game_active = True
+
+        while True:
+            self.ai_game._check_events()
+
+            if self.ai_game.stats.game_active:
+                self.ai_game.ship.update()
+                self.ai_game._update_bullets()
+                self.ai_game._update_aliens()
+                self.ai_game._fire_bullet()
+
+            self.ai_game._update_screen()
+
+if __name__ == '__main__':
+    ai_game = AlienInvasion()
+    ai_player = AIPlayer(ai_game)
+    ai_player.run_game()
