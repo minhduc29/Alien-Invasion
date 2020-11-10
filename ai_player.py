e0ddecf597460be828950b70b0796ebd6b98c743
Nguyen Minh DUc <ngmductm@gmail.com>
2020-11-11 06:30:02 +0700

    add automatic movement

--- ai_player.py
+++ ai_player.py
@@ -1,3 +1,4 @@
+import pygame
 from alien_invasion import AlienInvasion
 
 class AIPlayer:
@@ -8,17 +9,37 @@ class AIPlayer:
     def run_game(self):
         """Replace the original run_game()"""
         self.ai_game.stats.game_active = True
+        pygame.mouse.set_visible(False)
 
         while True:
             self.ai_game._check_events()
+            self._implement_strategy()
 
             if self.ai_game.stats.game_active:
                 self.ai_game.ship.update()
                 self.ai_game._update_bullets()
                 self.ai_game._update_aliens()
-                self.ai_game._fire_bullet()
 
             self.ai_game._update_screen()
+
+    def _implement_strategy(self):
+        """Implement an automated strategy for AI player"""
+        self._sweep_right_left()
+        self.ai_game._fire_bullet()
+
+    def _sweep_right_left(self):
+        """Move the ship right and left continuously"""
+        ship = self.ai_game.ship
+        screen_rect = self.ai_game.screen.get_rect()
+
+        if not ship.moving_right and not ship.moving_left:
+            ship.moving_right = True
+        elif ship.moving_right and ship.rect.right > screen_rect.right - 10:
+            ship.moving_right = False
+            ship.moving_left = True
+        elif ship.moving_left and ship.rect.left < 10:
+            ship.moving_left = False
+            ship.moving_right = True
 
 if __name__ == '__main__':
     ai_game = AlienInvasion()
